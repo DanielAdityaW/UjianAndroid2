e4848e2f2b54b1b59c2f1e84d5829ebe7c5e1086
Daniel Aditya <danieladitya38@gmail.com>
2024-10-20 20:11:48 +0700

    Update

--- app/src/main/java/com/bcafbootcamp/ujianandroid2/InputActivity.kt
+++ app/src/main/java/com/bcafbootcamp/ujianandroid2/InputActivity.kt
@@ -3,6 +3,7 @@ package com.bcafbootcamp.ujianandroid2
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -36,9 +37,14 @@ class InputActivity : AppCompatActivity() {
             val ost = inputOst.text.toString()
             val alamat = inputAlamat.text.toString()
 
-            val item = ModelItem(nama = nama, outstanding = ost, alamat = alamat)
-            viewModel.insert(item)
-            finish()
+            if (nama.isEmpty() || ost.isEmpty() || alamat.isEmpty()) {
+                Toast.makeText(this, "Data tidak boleh kosong", Toast.LENGTH_SHORT).show()
+            } else {
+                val item = ModelItem(nama = nama, outstanding = ost, alamat = alamat)
+                viewModel.insert(item)
+                finish()
+            }
+
         }
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
--- app/src/main/java/com/bcafbootcamp/ujianandroid2/UpdateActivity.kt
+++ app/src/main/java/com/bcafbootcamp/ujianandroid2/UpdateActivity.kt
@@ -3,6 +3,7 @@ package com.bcafbootcamp.ujianandroid2
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -47,9 +48,13 @@ class UpdateActivity : AppCompatActivity() {
             val ost = updateOst.text.toString()
             val alamat = updateAlamat.text.toString()
 
-            val item = ModelItem(id = itemId, nama = nama, outstanding = ost, alamat = alamat)
-            viewModel.update(item)
-            finish()
+            if (nama.isEmpty() || ost.isEmpty() || alamat.isEmpty()) {
+                Toast.makeText(this, "Data tidak boleh kosong", Toast.LENGTH_SHORT).show()
+            } else {
+                val item = ModelItem(nama = nama, outstanding = ost, alamat = alamat)
+                viewModel.update(item)
+                finish()
+            }
         }
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
